b4a19c6baa500fad31f6120f34632436e63fa80a
Anatoly <cy25rax@yandex.ru>
2023-04-05 22:51:08 +0500

    6 servlet tomcat

---
+++ untitled3/TomCat/src/main/java/org/example/servlets/FirstServlet.java
@@ -0,0 +1,17 @@
+package org.example.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		resp.getWriter().printf("<html><body>");
+		resp.getWriter().printf("<h1>Hello World </h1>");
+		resp.getWriter().printf("</html></body>");
+	}
+}
