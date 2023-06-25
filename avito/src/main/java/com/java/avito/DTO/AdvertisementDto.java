d49b83efa655279302b6d3776954eea01b141cde
Anatoly <cy25rax@yandex.ru>
2023-06-25 14:45:10 +0500

    каталог со списком товаров

---
+++ avito/src/main/java/com/java/avito/DTO/AdvertisementDto.java
@@ -0,0 +1,23 @@
+package com.java.avito.DTO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+public class AdvertisementDto {
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    private String categoryTitle;
+
+//    public AdvertisementDto(Long id, String title, BigDecimal price, String categoryTitle) {
+    public AdvertisementDto(Long id, String title, BigDecimal price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.categoryTitle = categoryTitle;
+    }
+}
---
+++ avito/src/main/java/com/java/avito/repositoryes/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.java.avito.repositoryes;
+
+import com.java.avito.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByTitle(String title);
+}
